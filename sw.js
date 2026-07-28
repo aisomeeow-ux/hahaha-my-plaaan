b91ac1590864b008ad16097dbf06c29174727d87
aisomeeow-ux <aiso.meeow@gmail.com>
2026-07-28 18:25:21 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ritual-tracker-v1';
+const CACHE_NAME = 'ritual-tracker-v2';
 const FILES_TO_CACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
